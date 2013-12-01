222fe636ae268dc3956d126d068da99cd5d050b9
coolhaoming <coolhaoming@gmail.com>
2013-11-30 21:11:21 -0800

    Add root route to fix heroku error

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ FirstApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root_path -> '/'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
